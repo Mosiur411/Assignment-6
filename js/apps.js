0500fa59a7f79d45e59089f5c6e932c95624d300
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-03-01 18:10:32 +0600

    complid

--- js/apps.js
+++ js/apps.js
@@ -1,32 +1,41 @@
+document.getElementById("lodingarea").style.display="none"
+/* iputvakye clear */
+const input =()=>{
+    const inputvalue = document.getElementById("input-value").value;
+    return inputvalue;
+}
 /* search click me */
 const searchclick = () =>{
+    document.getElementById("lodingarea").style.display="block"// spinner 
     document.getElementById("erro").style.display ="none"
     document.getElementById("Mobile-area").innerHTML='';
     document.getElementById("Detail-area-display").innerHTML=''
-    const input = document.getElementById("input-value");
-    const inputvalue = input.value;
+    const inputvalue = input()
     if(inputvalue == ''){
         document.getElementById("erro").style.display ="block"
+        document.getElementById("lodingarea").style.display="none"// spinner 
     }else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalue}`
         fetch(url)
         .then(res => res.json())
-        .then(data => display(data.data.slice(0,20)))
-        input.value=''
+        .then(data => display(data.data.slice(0,19)))
     } 
 }
-/* search display result */
+/* search result and data */
 const display = Mobile =>{
+    document.getElementById("lodingarea").style.display="none"// spinner 
     if(Mobile.length == 0){
-        document.getElementById("erro").style.display ="block"
-    }else{
+       const erro = document.getElementById("erro");
+       erro.innerText ="Did Not Mobile"
+       erro.style.display ="block"
+    }
+    else{
         Mobile.forEach(Mobiles => {
             const Mobilearea = document.getElementById("Mobile-area");
             const div =document.createElement("div");
-            div.style.paddingTop ="40px"
-            div.style.paddingBottom ="40px"
+            div.style.padding="40px 0"
             div.innerHTML=`<div class="card pt-3">
-            <img src="${Mobiles.image}" class="card-img-top w-50 mx-auto" alt="not images">
+            <img src="${Mobiles.image}" class="card-img-top w-50 mx-auto" alt="not a images">
             <div class="card-body">
                 <h3>Names: ${Mobiles.phone_name}</h3>
                 <h3>Brand: ${Mobiles.brand}</h3>
@@ -34,16 +43,20 @@ const display = Mobile =>{
             </div>
             </div>`
             Mobilearea.appendChild(div)
-        //    const allPhone = document.getElementById("phoneall");
-        //    allPhone.style.display="block"
-        //    allPhone.addEventListener("click", function (Mobiles){
-        //     document.getElementById("erro").style.display ="block"
-
-        //    })
+           /* All Phone id   */
+           document.getElementsByClassName("allPhoneshow")[0].style.display ="block"
         });
     }
 }
-/* display Detail */
+/* All Phone */
+const allPhone =()=>{
+    const inputvalue = input()
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalue}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>display(data.data.slice(20,1000)))
+}
+/* Detail data load */
 const Detailclick = Detail =>{
     document.getElementById("Detail-area-display").innerHTML=''
     const Details =`https://openapi.programming-hero.com/api/phone/${Detail}`
@@ -51,11 +64,12 @@ const Detailclick = Detail =>{
     .then(res => res.json())
     .then(data =>displayDetail(data.data))
 }
+/* Detail displow show */
 const displayDetail =(data)=>{
     const Detailresult = document.getElementById("Detail-area-display");
     const div = document.createElement("div");
     div.innerHTML =`<div class="card p-4">
-        <img src="${data.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <img src="${data.image}" class="card-img-top w-50 mx-auto" alt="not a images">
         <div class="card-body">
             <h3>Names: ${data.name}</h3>
             <h3>Brand: ${data.brand}</h3>
@@ -72,6 +86,6 @@ const displayDetail =(data)=>{
             <strong>${data.mainFeatures.sensors[5]}</strong>
         </div>
     </div>
-    `
+    ` 
     Detailresult.appendChild(div)
 }
