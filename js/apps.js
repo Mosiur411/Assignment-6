bcf7594c63b4f632df9fd3b7c91de331a48a1712
Mosiur islam <Mosiurislamwebdesing@gmail.com>
2022-03-01 21:51:42 +0600

    complide

--- js/apps.js
+++ js/apps.js
@@ -1,60 +1,61 @@
 document.getElementById("lodingarea").style.display="none"
-/* iputvakye clear */
-const input =()=>{
-    const inputvalue = document.getElementById("input-value").value;
-    return inputvalue;
-}
 /* search click me */
 const searchclick = () =>{
-    document.getElementById("lodingarea").style.display="block"// spinner 
+    document.getElementById("lodingarea").style.display="block"
     document.getElementById("erro").style.display ="none"
     document.getElementById("Mobile-area").innerHTML='';
     document.getElementById("Detail-area-display").innerHTML=''
-    const inputvalue = input()
+    const input = document.getElementById("input-value");
+    const inputvalue = input.value;
     if(inputvalue == ''){
         document.getElementById("erro").style.display ="block"
-        document.getElementById("lodingarea").style.display="none"// spinner 
+        document.getElementById("lodingarea").style.display="none"
+        document.getElementsByClassName("allPhoneshow")[0].style.display="none"
     }else{
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalue}`
         fetch(url)
         .then(res => res.json())
-        .then(data => display(data.data.slice(0,19)))
-    } 
+        .then(data => display(data.data))
+    }
+    input.value =''
 }
 /* search result and data */
 const display = Mobile =>{
-    document.getElementById("lodingarea").style.display="none"// spinner 
+    document.getElementById("lodingarea").style.display="none"
     if(Mobile.length == 0){
        const erro = document.getElementById("erro");
        erro.innerText ="Did Not Mobile"
        erro.style.display ="block"
     }
+    else if(Mobile.length >= 20){
+        clickmobile(Mobile.slice(0,20))
+        const allbutton = document.getElementsByClassName("allPhoneshow")[0];
+        allbutton.style.display ="block"
+        allbutton.addEventListener("click", function (){
+            clickmobile(Mobile)
+        })
+    }
     else{
-        Mobile.forEach(Mobiles => {
-            const Mobilearea = document.getElementById("Mobile-area");
-            const div =document.createElement("div");
-            div.style.padding="40px 0"
-            div.innerHTML=`<div class="card pt-3">
-            <img src="${Mobiles.image}" class="card-img-top w-50 mx-auto" alt="not a images">
-            <div class="card-body">
-                <h3>Names: ${Mobiles.phone_name}</h3>
-                <h3>Brand: ${Mobiles.brand}</h3>
-              <button class="btn btn-primary px-4" onclick="Detailclick('${Mobiles.slug}')">Detail</button>
-            </div>
-            </div>`
-            Mobilearea.appendChild(div)
-           /* All Phone id   */
-           document.getElementsByClassName("allPhoneshow")[0].style.display ="block"
-        });
+       
+        clickmobile(Mobile)
     }
 }
-/* All Phone */
-const allPhone =()=>{
-    const inputvalue = input()
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputvalue}`
-        fetch(url)
-        .then(res => res.json())
-        .then(data =>display(data.data.slice(20,1000)))
+const clickmobile = Mobiledata =>{
+    Mobiledata.forEach(Mobiles => {
+        const Mobilearea = document.getElementById("Mobile-area");
+        const div =document.createElement("div");
+        div.style.padding="40px 0"
+        div.innerHTML=`<div class="card pt-3">
+        <img src="${Mobiles.image}" class="card-img-top w-50 mx-auto" alt="not a images">
+        <div class="card-body">
+            <h3>Names: ${Mobiles.phone_name}</h3>
+            <h3>Brand: ${Mobiles.brand}</h3>
+          <button class="btn btn-primary px-4" onclick="Detailclick('${Mobiles.slug}')">Detail</button>
+        </div>
+        </div>`
+        Mobilearea.appendChild(div)
+    });
+    
 }
 /* Detail data load */
 const Detailclick = Detail =>{
